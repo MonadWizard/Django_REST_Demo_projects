750cc6c7b9cd353526096874fc83439660f1c90e
MonadWizard <monad.wizard.r@gmail.com>
2020-11-01 01:15:09 +0600

    0.01_REST_without_UI

---
+++ REST_TODO/api/views.py
@@ -0,0 +1,72 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import Task
+from .serializers import TaskSerializer
+# Create your views here.
+
+@api_view(['GET'])
+def apiOverview(request):
+
+    api_urls = {
+        'list': '/task-list/',
+        'detail view': '/task-detail/<str:pk>/',
+        'Create': '/task-create/',
+        'Update': '/task-update/<str:pk>/',
+        'Delete': '/task-delete/<str:pk>/',
+
+    }
+   
+    return Response(api_urls)
+
+@api_view(['GET'])
+def taskList(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def taskDetail(request, pk):
+    tasks = Task.objects.get(id=pk)
+    serializer = TaskSerializer(tasks, many=False)
+
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def taskCreate(request):
+	serializer = TaskSerializer(data=request.data)
+
+	if serializer.is_valid():
+		serializer.save()
+
+	return Response(serializer.data)
+
+@api_view(['POST'])
+def taskUpdate(request, pk):
+	task = Task.objects.get(id=pk)
+	serializer = TaskSerializer(instance=task, data=request.data)
+
+	if serializer.is_valid():
+		serializer.save()
+
+	return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def taskDelete(request, pk):
+	task = Task.objects.get(id=pk)
+	task.delete()
+
+	return Response('Item succsesfully delete!')
+
+
+
+
+
+
+
